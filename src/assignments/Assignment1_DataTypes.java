d6909073258cf71c0f32372589e42b5ff6354b65
Saurabh Kawade <saurabhkawade00027@gmail.com>
2025-12-08 23:41:11 +0530

    Hello world

--- src/assignments/Assignment1_DataTypes.java
+++ src/assignments/Assignment1_DataTypes.java
@@ -3,7 +3,8 @@ package assignments;
 public class Assignment1_DataTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Hello World
+		System.out.println("Hello World");
 
 	}
 
